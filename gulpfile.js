9e4de4cd9502a52b5077087cd08a85fbf1fd1c35
Michael Himing <mhiming@gmail.com>
2019-02-12 22:15:42 +1100

    gulp 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,11 @@
-const babel = require('gulp-babel');
 const fs = require('fs');
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 const cmdRegPack = require('regpack').cmdRegPack;
 
-gulp.task('default', ['join_assets'], function () {
-  gulp.watch('src/*', ['join_assets']);
-});
-
-gulp.task('join_assets', ['regpack'], function () {
-  const js = fs.readFileSync('dist/packed.js', 'utf8');
-  return gulp.src('src/shim.html')
-    .pipe(replace(/<demo>/g, js))
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('regpack', ['minify'], function () {
-  const js = fs.readFileSync('dist/min.js', 'utf8');
-  const packed = cmdRegPack(js, {reassignVars: false});
-  fs.writeFileSync('dist/packed.js', packed);
-});
-
-gulp.task('minify', function () {
+function minify() {
   return gulp.src('src/full.js')
     .pipe(babel({
       compact: true,
@@ -35,4 +17,28 @@ gulp.task('minify', function () {
     .pipe(replace(/;(?=}|$)/g, ''))
     .pipe(rename('min.js'))
     .pipe(gulp.dest('dist'));
-});
+}
+
+function regpack(cb) {
+  const js = fs.readFileSync('dist/min.js', 'utf8');
+  const packed = cmdRegPack(js, { reassignVars: false });
+  fs.writeFileSync('dist/packed.js', packed);
+  cb();
+}
+
+function join() {
+  const js = fs.readFileSync('dist/packed.js', 'utf8');
+  return gulp.src('src/shim.html')
+    .pipe(replace(/<demo>/g, js))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'));
+}
+
+const build = gulp.series(minify, regpack, join)
+
+function watch() {
+  gulp.watch(['src/*'], build)
+}
+
+exports.default = build;
+exports.watch = gulp.series(build, watch);
