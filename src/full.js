2562d768ab691b0cd89e48f2e1692f05a3d412ee
Michael Himing <mhiming@gmail.com>
2019-02-15 17:05:23 +1100

    Change the X

--- src/full.js
+++ src/full.js
@@ -1,22 +1,21 @@
 n = document.createElement('canvas'),
     o = n.getContext`2d`,
     I = c.createImageData(256, 256),
-    I.data.fill(t = n.width = n.height = 256),
+    I.data.fill(t = n.width = n.height = 256)
 
 setInterval(_ => {
     for (i = ++t % 7; i < 256 * 256; i += 7)
         x = i % 256 - 128,
             y = i / 256 - 128,
             // http://www.iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
-            k = Math.max(Math.max(Math.abs(x - y), Math.abs(x + y)) - 99, Math.min(Math.abs(x - y), Math.abs(x + y))) % 27 < 20,
+            k = Math.abs(30 - Math.max(Math.max(Math.abs(x - y), Math.abs(x + y)) - 99, Math.min(Math.abs(x - y), Math.abs(x + y)))) > 10,
             v = Math.sin(x * Math.cos(t / 99) / 33 - y * Math.sin(t / 99) / 33 + y * Math.cos(t / 99) / 33 + x * Math.sin(t / 99) / 33)
             + Math.sin(y / 33)
             + Math.sin(x / 20)
-            + Math.sin(Math.hypot(x + 99 * Math.cos(t / 33), y + 99 * Math.sin(t / 33)) / 20)
-            + 22 * k,
-            I.data[i * 4] = 127 * k + 127 * Math.cos(t / 30 + v),
-            I.data[i * 4 + 1] = 127 * k + 127 * Math.cos(t / 10 + v * 2),
-            I.data[i * 4 + 2] = 127 * k + 127 * Math.cos(t / 13 + v * 3)
+            + Math.sin(Math.hypot(x + 99 * Math.cos(t / 33), y + 99 * Math.sin(t / 33)) / 20),
+            I.data[i * 4] = 127 * k + 127 * Math.cos(t / 30 + v + 22 * k),
+            I.data[i * 4 + 1] = 127 * k + 127 * Math.cos(t / 10 + v * 2 + 22 * k),
+            I.data[i * 4 + 2] = 127 * k + 127 * Math.cos(t / 13 + v * 3 + 22 * k)
 
     o.putImageData(I, 0, 0)
 
