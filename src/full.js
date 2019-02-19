a55dcce0d23a3789e40a3d8e8a5b3892a1f307c7
Michael Himing <mhiming@gmail.com>
2019-02-20 10:51:57 +1100

    Globalalpha for lighting, animate xbox

--- src/full.js
+++ src/full.js
@@ -1,13 +1,15 @@
 n = document.createElement`canvas`
 o = n.getContext`2d`
-I = c.createImageData(256, 256), I.data.fill(t = n.height = 256)
+I = c.createImageData(256, 256), I.data.fill(n.height = 256)
+
+t = 0
 
 setInterval(_ => {
-    if (t % 500 < 300)
+    if (t % 500 < 250)
         for (i = 0; i < 256 * 256; i += 1)
             x = i % 256 - 128,
                 y = i / 256 - 128,
-                I.data[i * 4] = I.data[i * 4 + 1] = I.data[i * 4 + 2] = Math.max(Math.min(Math.abs(x - y), Math.abs(x + y)), Math.max(Math.abs(x - y), Math.abs(x + y)) - 99) % 30 < 20 ? 0 : 256
+                I.data[i * 4] = I.data[i * 4 + 1] = I.data[i * 4 + 2] = Math.max(Math.min(Math.abs(x - y), Math.abs(x + y)), Math.max(Math.abs(x - y), Math.abs(x + y)) - 99) % 30 < 20 && Math.max(Math.min(Math.abs(x - y), Math.abs(x + y)), Math.max(Math.abs(x - y), Math.abs(x + y)) - 99) < t % 500 * 2 ? 0 : 256
     else
         for (i = t % 7; i < 256 * 256; i += 7)
             x = i % 256 - 128,
@@ -22,9 +24,8 @@ setInterval(_ => {
                 I.data[i * 4 + 1] = 127 * k + 127 * Math.cos(t / 10 + v * 2 + Math.PI * k),
                 I.data[i * 4 + 2] = 127 * k + 127 * Math.cos(t / 13 + v * 3 + Math.PI * k)
 
-
-    c.resetTransform(o.putImageData(I, 0, 0))
-    c.fillRect(0, 0, 512, 512)
+    c.resetTransform(c.globalAlpha = 1)
+    c.fillRect(~o.putImageData(I, 0, 0), 0, 512, 512)
 
     // front/back
     c.setTransform(
@@ -34,7 +35,7 @@ setInterval(_ => {
         Math.cos(t / 99 /*pitch*/),
         256 + Math.sin(t / 33 /*yaw*/) * Math.sign(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/)) * 128,
         256 - Math.cos(t / 33 /*yaw*/) * Math.sin(t / 99 /*pitch*/) * Math.sign(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/)) * 128)
-    c.filter = 'brightness(' + Math.abs(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/))
+    c.globalAlpha = Math.abs(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/))
     c.drawImage(n, -128, -128)
 
     // left/right
@@ -45,7 +46,7 @@ setInterval(_ => {
         Math.cos(t / 99 /*pitch*/),
         256 - Math.cos(t / 33 /*yaw*/) * Math.sign(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/)) * 128,
         256 - Math.sin(t / 33 /*yaw*/) * Math.sin(t / 99 /*pitch*/) * Math.sign(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/)) * 128)
-    c.filter = 'brightness(' + Math.abs(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/))
+    c.globalAlpha = Math.abs(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/))
     c.drawImage(n, -128, -128)
 
     // top/bottom
@@ -56,7 +57,7 @@ setInterval(_ => {
         Math.cos(t / 33 /*yaw*/) * Math.sin(t / 99 /*pitch*/),
         256,
         256 + Math.cos(t / 99 /*pitch*/) * Math.sign(Math.sin(t / 99 /*pitch*/)) * 128)
-    c.filter = 'brightness(' + Math.abs(Math.sin(t / 99 /*pitch*/))
+    c.globalAlpha = + Math.abs(Math.sin(t / 99 /*pitch*/))
     c.drawImage(n, -128, -128)
 
     t++
