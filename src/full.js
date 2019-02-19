933a28f33f73e2a4a071e0cfcb7fe4f8017f8413
Michael Himing <mhiming@gmail.com>
2019-02-19 13:17:10 +1100

    526 xbox

--- src/full.js
+++ src/full.js
@@ -1,7 +1,20 @@
-t = setInterval(_ => {
+n = document.createElement`canvas`
+o = n.getContext`2d`
+n.width = n.height = 256
+
+for (t = 12; --t; o.stroke())
+    o.strokeStyle = t & 1 ? '#000' : '#fff',
+        o.lineWidth = (t + (t + 1 >> 1)) * 14.14,
+        o.beginPath(),
+        o.moveTo(128 - 50 - (t + (t + 1 >> 1)) * 5, 128 - 50 - (t + (t + 1 >> 1)) * 5),
+        o.lineTo(128 + 50 + (t + (t + 1 >> 1)) * 5, 128 + 50 + (t + (t + 1 >> 1)) * 5),
+        o.moveTo(128 - 50 - (t + (t + 1 >> 1)) * 5, 128 + 50 + (t + (t + 1 >> 1)) * 5),
+        o.lineTo(128 + 50 + (t + (t + 1 >> 1)) * 5, 128 - 50 - (t + (t + 1 >> 1)) * 5)
+
+
+setInterval(_ => {
     t++
-    c.resetTransform()
-    c.fillStyle = '#000'
+    c.resetTransform(c.globalAlpha = 1)
     c.fillRect(0, 0, 512, 512)
 
     // front/back
@@ -12,8 +25,8 @@ t = setInterval(_ => {
         Math.cos(t / 99 /*pitch*/),
         256 + Math.sin(t / 33 /*yaw*/) * Math.sign(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/)) * 128,
         256 - Math.cos(t / 33 /*yaw*/) * Math.sin(t / 99 /*pitch*/) * Math.sign(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/)) * 128)
-    c.fillStyle = 'rgb(' + ~~Math.abs(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/) * 255) + ',' + ~~Math.abs(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/) * 255) + ',' + ~~Math.abs(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/) * 255) + ')'
-    c.fillRect(-128, -128, 256, 256)
+    c.globalAlpha = Math.abs(Math.cos(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/))
+    c.drawImage(n, -128, -128)
 
     // left/right
     c.setTransform(
@@ -23,8 +36,8 @@ t = setInterval(_ => {
         Math.cos(t / 99 /*pitch*/),
         256 - Math.cos(t / 33 /*yaw*/) * Math.sign(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/)) * 128,
         256 - Math.sin(t / 33 /*yaw*/) * Math.sin(t / 99 /*pitch*/) * Math.sign(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/)) * 128)
-        c.fillStyle = 'rgb(' + ~~Math.abs(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/) * 255) + ',' + ~~Math.abs(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/) * 255) + ',' + ~~Math.abs(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/) * 255) + ')'
-    c.fillRect(-128, -128, 256, 256)
+    c.globalAlpha = Math.abs(Math.sin(t / 33 /*yaw*/) * Math.cos(t / 99 /*pitch*/))
+    c.drawImage(n, -128, -128)
 
     // top/bottom
     c.setTransform(
@@ -34,6 +47,6 @@ t = setInterval(_ => {
         Math.cos(t / 33 /*yaw*/) * Math.sin(t / 99 /*pitch*/),
         256,
         256 + Math.cos(t / 99 /*pitch*/) * Math.sign(Math.sin(t / 99 /*pitch*/)) * 128)
-        c.fillStyle = 'rgb(' + ~~Math.abs(Math.sin(t / 99 /*pitch*/) * 255) + ',' + ~~Math.abs(Math.sin(t / 99 /*pitch*/) * 255) + ',' + ~~Math.abs(Math.sin(t / 99 /*pitch*/) * 255) + ')'
-    c.fillRect(-128, -128, 256, 256)
+    c.globalAlpha = Math.abs(Math.sin(t / 99 /*pitch*/))
+    c.drawImage(n, -128, -128)
 }, 33)
